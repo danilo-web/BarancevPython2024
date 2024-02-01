0ea5a840d7630d605951c75abc4b1182c7727423
danie <danilo-web@mail.ru>
2024-02-01 13:35:59 -0500

    добавил if-else в

--- test/test_edit_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_delete_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="EDITED", header="EDITED", footer="EDITED"))
-    app.session.logout()
