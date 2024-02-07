b41d3b1087acbdd9960011951bc6952e475d42c7
danie <danilo-web@mail.ru>
2024-02-07 09:30:27 -0500

    перешел на XAMPP 7.2 , реализовано сравнения списков для групп

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,3 +6,10 @@ def test_create_new_contact(app):
                                nickname="test", title="twst", company="test", address="test", home="77777777",
                                mobile="888888888", work="9999999", fax="555555", email="test", email2="test",
                                email3="test"))
+
+
+def test_create_new_another_contact(app):
+    app.contact.create(Contact(firstname="ABC", middlename="ABC", lastname="ABC",
+                               nickname="ABC", title="ABC", company="ABC", address="test", home="ABC",
+                               mobile="ABC", work="ABC", fax="ABC", email="ABC", email2="ABC",
+                               email3="ABC"))
