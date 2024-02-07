acd84a6a2af0c4234a9fbd74be9eae19ed9c0e5c
danie <danilo-web@mail.ru>
2024-02-07 17:00:20 -0500

    реализованы ассершены для всех тестов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,4 +4,12 @@ from model.contact import Contact
 def test_delete_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+    print(new_contacts)
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -4,4 +4,16 @@ from model.contact import Contact
 def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    app.contact.modify_first_contact(Contact(firstname="NEW_UPDATED_EDITED"))
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="NO", lastname="NO")
+    contact.element_id = old_contacts[0].element_id
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    for i in range(len(old_contacts)):
+        if old_contacts[i].element_id == contact.element_id:
+            old_contacts[i] = contact
+            break
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+    print(new_contacts)
