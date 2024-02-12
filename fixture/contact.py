2292fe301687e0d9232c36e25e076eeb8cd7be85
danie <danilo-web@mail.ru>
2024-02-12 12:32:09 -0500

    test phones  создан второй тест, оба работают

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 import time
+import re
 from model.contact import Contact
 
 
@@ -48,6 +49,13 @@ class ContactHelper:
         wd = self.app.wd
         if not wd.current_url.endswith("/"):
             wd.find_element_by_link_text("home").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_contact_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -109,3 +117,13 @@ class ContactHelper:
         workphone = wd.find_element_by_name("work").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, element_id=element_id, homephone=homephone,
                        mobilephone=mobilephone, workphone=workphone)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        return Contact(homephone=homephone, mobilephone=mobilephone, workphone=workphone)
+
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,3 +1,4 @@
+import re
 from model.contact import Contact
 
 
@@ -6,7 +7,25 @@ def test_phone_on_home_page(app):
         app.contact.create(Contact(firstname="test", lastname="test"))
     contact_from_homepage = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_homepage.homephone == contact_from_edit_page.homephone
-    assert contact_from_homepage.mobilephone == contact_from_edit_page.mobilephone
-    assert contact_from_homepage.workphone == contact_from_edit_page.workphone
-    assert contact_from_homepage.fax_phone == contact_from_edit_page.fax_phone
+    assert contact_from_homepage.homephone == clear(contact_from_edit_page.homephone)
+    assert contact_from_homepage.mobilephone == clear(contact_from_edit_page.mobilephone)
+    assert contact_from_homepage.workphone == clear(contact_from_edit_page.workphone)
+    assert contact_from_homepage.fax_phone == clear(contact_from_edit_page.fax_phone)
+
+
+def test_phone_on_contact_view_page(app):
+    # if app.contact.count() == 0:
+    #     app.contact.create(Contact(firstname="test", lastname="test"))
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)  # [0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+    assert contact_from_view_page.fax_phone == contact_from_edit_page.fax_phone
+
+
+def clear(s):
+    if s is None:
+        return None
+    return re.sub("[() -]", "", s)
+
