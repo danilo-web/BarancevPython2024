30ddae536a7eaa3f92c1c1616b9035315f1c2db3
danie <danilo-web@mail.ru>
2024-02-12 23:23:50 -0500

    Homework 14 (5.1) - fixes

--- test/test_home_page_asserts.py
+++ test/test_home_page_asserts.py
@@ -12,29 +12,17 @@ def test_contact_data_on_home_page(app):
     index = randrange(len(app.contact.get_contact_list()))
     contact_from_homepage = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-
-    print('\n' "id = " + str(index))
-    print("from_homepage:  " + '\n' + str(contact_from_homepage.all_phones_from_homepage) + '\n' +
-                                      str(contact_from_homepage.all_emails_from_homepage))
-    print("from_edit_page: " + '\n' + str(merge_phones_like_on_homepage(contact_from_edit_page) + '\n' +
-                                      str(merge_emails_like_on_homepage(contact_from_edit_page))))
-
     assert contact_from_homepage.firstname == contact_from_edit_page.firstname
     assert contact_from_homepage.lastname == contact_from_edit_page.lastname
     assert contact_from_homepage.address == contact_from_edit_page.address
     assert contact_from_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(contact_from_edit_page)
     assert contact_from_homepage.all_emails_from_homepage == merge_emails_like_on_homepage(contact_from_edit_page)
 
-
-# def test_phone_on_contact_view_page(app):
-#     # if app.contact.count() == 0:
-#     #     app.contact.create(Contact(firstname="test", lastname="test"))
-#     contact_from_view_page = app.contact.get_contact_from_view_page(0)  # [0]
-#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-#     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
-#     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-#     assert contact_from_view_page.fax_phone == contact_from_edit_page.fax_phone
+    print('\n' "id = " + str(index))
+    print("from_homepage:  " + '\n' + str(contact_from_homepage.all_phones_from_homepage) + '\n' +
+                                      str(contact_from_homepage.all_emails_from_homepage))
+    print("from_edit_page: " + '\n' + str(merge_phones_like_on_homepage(contact_from_edit_page) + '\n' +
+                                      str(merge_emails_like_on_homepage(contact_from_edit_page))))
 
 
 def clear(s):
@@ -50,8 +38,25 @@ def merge_phones_like_on_homepage(contact):
                                        [contact.homephone, contact.mobilephone, contact.workphone]))))
 
 
+def clear_emails(s):
+    if s is None:
+        return None
+    return re.sub("[() !?]", "", s)
+
+
 def merge_emails_like_on_homepage(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
+                            map(lambda x: clear_emails(x),
                                 filter(lambda x: x is not None,
                                        [contact.email, contact.email2, contact.email3]))))
+
+
+# def test_phone_on_contact_view_page(app):
+#     # if app.contact.count() == 0:
+#     #     app.contact.create(Contact(firstname="test", lastname="test"))
+#     contact_from_view_page = app.contact.get_contact_from_view_page(0)  # [0]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+#     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+#     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+#     assert contact_from_view_page.fax_phone == contact_from_edit_page.fax_phone
