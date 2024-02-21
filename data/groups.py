bf13953e96959990547414ed3357371fa513b3dd
danie <danilo-web@mail.ru>
2024-02-21 17:11:22 -0500

    полностью реализована  jsonpickle для group на 6м уроке

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+testdata = [
+    Group(name="testdata", header="testdata", footer="testdata"),
+    Group(name="testdata2", header="testdata2", footer="testdata2"),
+]
--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import string
 import random
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -13,7 +13,7 @@ except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
-n = 5
+n = 6
 f = "data/groups.json"
 
 for o, a in opts:
@@ -35,7 +35,9 @@ testdata = [Group(name="", header="", footer="")] + [
 ]
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
-print("Путь к файлу:", file)
+print("Путь к файлу:", file)  # можно добавить конфигурацию -n 10 -f data/test.json через Script Parameters
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
+
